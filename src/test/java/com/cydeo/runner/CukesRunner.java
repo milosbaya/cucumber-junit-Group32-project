f2d18c0a731a30aae4db02fe4c9ffb23d605b7da
Galina Klaus <galina.n.klaus@gmail.com>
2022-07-06 11:42:38 -0400

    First changes

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,11 +8,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin ={"html:target/cucumber-report.html",
-        "json:target/cucumber-reports/cucumber.json"},
+                "json:target/cucumber-report.json",
+                "rerun:target/rerun.txt",
+                "pretty"},
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions" ,
         dryRun = false,
-        tags ="@vytrack_resfresh_btn and @B26G32-60 and @B26G32-61"
+        tags ="@vytrack_resfresh_btn and @B26G32-60 and @B26G32-61",
+        monochrome = true
 )
 
 public class CukesRunner {
