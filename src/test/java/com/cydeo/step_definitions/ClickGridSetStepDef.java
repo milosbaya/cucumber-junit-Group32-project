bfaa266d13b352814c661a31421f143f018531dd
Nigar <npanahova@gmail.com>
2022-07-10 16:47:14 -0400

    partly completed Scenario for store manager

--- src/test/java/com/cydeo/step_definitions/ClickGridSetStepDef.java
+++ src/test/java/com/cydeo/step_definitions/ClickGridSetStepDef.java
@@ -6,6 +6,7 @@ import com.cydeo.pages.Vytrack_pages.VehiclesPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -34,13 +35,16 @@ public class ClickGridSetStepDef {
 
     @Given("User is on VyTrack homepage")
     public void user_is_on_vy_track_homepage() {
-
+        String expectedTitle = "Dashboard";
+        wait= new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.titleIs(expectedTitle));
+        Assert.assertEquals(expectedTitle, driver.getTitle());
     }
     @Given("User clicks on Fleet")
     public void user_clicks_on_fleet() {
-        BrowserUtils.waitForClickablility(dashboardPage.fleetBtn_manager, 20);
-
-       dashboardPage.fleetBtn_manager.click();
+        BrowserUtils.waitForClickablility(dashboardPage.getFleetBtn(username), 20);
+        BrowserUtils.waitFor(5);
+       dashboardPage.getFleetBtn(username).click();
     }
     @Given("User clicks on Vehicles under Fleet dropdown")
     public void user_clicks_on_vehicles_under_fleet_dropdown() {
@@ -56,8 +60,8 @@ public class ClickGridSetStepDef {
     }
     @When("the Store manager sees Grid Settings is on the right side of the Reset button and on the right side of the page")
     public void the_store_manager_sees_grid_settings_is_on_the_right_side_of_the_reset_button_and_on_the_right_side_of_the_page() {
-        boolean btn = vehiclesPage.IfBtnOnTheLeft("Grid Settings", "Reset");
-        Assert.assertTrue("Grid Settings Button position is  failed " , vehiclesPage.Grid.isDisplayed() );
+        boolean btn = vehiclesPage.isTheElementOnTheRIGHTSideOfPage(vehiclesPage.Grid);
+        Assert.assertTrue("Grid Settings Button position is  failed " , vehiclesPage.isTheElementOnTheRIGHTSideOfPage(vehiclesPage.Grid) );
         Assert.assertTrue(" Grid Settings Button assertion failed", btn );
 
     }
@@ -68,4 +72,19 @@ public class ClickGridSetStepDef {
     }
 
 
+
+
+    @And("the Store manager sees {string}, {string}, {string}")
+    public void theStoreManagerSees(String arg0, String arg1, String arg2) {
+        Assert.assertEquals("Verification failed","NAME",vehiclesPage.nameSM.getText());
+        Assert.assertEquals("Verification failed","SORT",vehiclesPage.sortS.getText());
+       Assert.assertEquals("Verification failed","SHOW",vehiclesPage.show.getText());
+
+       Assert.assertTrue(vehiclesPage.nameSM.isDisplayed());
+        Assert.assertTrue(vehiclesPage.show.isDisplayed());
+        Assert.assertTrue(vehiclesPage.sortS.isDisplayed());
+
+
+
+    }
 }
--- src/test/resources/features/ClickGridButton.feature
+++ src/test/resources/features/ClickGridButton.feature
@@ -10,3 +10,4 @@ Feature: As a store manager I should be able to click Grid Settings button
     And the Store manager is on Vehicle page
     When the Store manager sees Grid Settings is on the right side of the Reset button and on the right side of the page
     Then the Store manager should be able to click Grid Settings button
+    And the Store manager sees "Name", "Sort", "Show"
